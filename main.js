544a29d683bd0b8b151bd96fadc8b3d9595a7044
Lachezar Damyanov <e12319590@student.tuwien.ac.at>
2025-07-27 13:15:40 +0200

    Created main window logic and renderer

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log("Hello World!");
+// Backend of the Save Reminder app
+// This file is responsible for the main process of the Electron application
+
+const path = require('path');
+const { app, BrowserWindow } = require('electron');
+
+const createMainWindow = () => {
+	const mainWindow = new BrowserWindow({
+		width: 400,
+		height: 500
+	})
+
+	mainWindow.loadFile(path.join(__dirname, './renderer/index.html'));
+}
+
+app.whenReady().then(() => {
+	createMainWindow()
+});
